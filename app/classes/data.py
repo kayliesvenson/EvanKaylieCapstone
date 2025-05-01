f02d16b10b28742136ccd1f0faa4bf92efdc9b08
KaylieSvenson <kaylielaurence@gmail.com>
2025-05-01 12:18:33 -0700

    s

--- app/classes/data.py
+++ app/classes/data.py
@@ -63,38 +63,6 @@ class Question(Document):
         'ordering': ['-createdate']
     }
 
-class Comment(Document):
-    # Line 63 is a way to access all the information in Course and Teacher w/o storing it in this class
-    author = ReferenceField('User',reverse_delete_rule=CASCADE) 
-    blog = ReferenceField('Blog',reverse_delete_rule=CASCADE)
-    # This could be used to allow comments on comments
-    comment = ReferenceField('Comment',reverse_delete_rule=CASCADE)
-    # Line 68 is where you store all the info you need but won't find in the Course and Teacher Object
-    content = StringField()
-    create_date = DateTimeField(default=dt.datetime.utcnow)
-    modify_date = DateTimeField()
-
-    meta = {
-        'ordering': ['-createdate']
-    }
-
-class Clinic(Document):
-    author = ReferenceField('User',reverse_delete_rule=CASCADE) 
-    createdate = DateTimeField(default=dt.datetime.utcnow)
-    modifydate = DateTimeField()
-    name = StringField()
-    streetAddress = StringField()
-    city = StringField()
-    state = StringField()
-    zipcode = StringField()
-    description = StringField()
-    lat = FloatField()
-    lon = FloatField()
-    
-    meta = {
-        'ordering': ['-createdate']
-    }
-
 class Reply(Document):
     # Line 63 is a way to access all the information in Course and Teacher w/o storing it in this class
     author = ReferenceField('User',reverse_delete_rule=CASCADE) 
